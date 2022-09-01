c5ecbe4db59e6f840c7b16e1d84ff3c7fb15f8e6
scliang-strive <354767929@qq.com>
2022-09-01 12:15:01 +0800

    19-删除链表的倒数第n个结点

--- leetcode/19-删除链表的倒数第 N 个结点/19-删除链表的倒数第 N 个结点.go
+++ leetcode/19-删除链表的倒数第 N 个结点/19-删除链表的倒数第 N 个结点.go
@@ -8,7 +8,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// 删除链表的倒数第n个结点
+// 19-删除链表的倒数第n个结点
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	dummy := &ListNode{Val: -1, Next: head}
 	fast, slow := head, dummy
