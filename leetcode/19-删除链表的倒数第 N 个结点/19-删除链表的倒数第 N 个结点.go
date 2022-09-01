4f73bf53e8175ed460fe4946937a13f4f55f3142
scliang-strive <354767929@qq.com>
2022-09-01 12:14:32 +0800

    删除链表的倒数第n个结点

---
+++ leetcode/19-删除链表的倒数第 N 个结点/19-删除链表的倒数第 N 个结点.go
@@ -0,0 +1,23 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 删除链表的倒数第n个结点
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Val: -1, Next: head}
+	fast, slow := head, dummy
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for ; fast != nil; fast = fast.Next {
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
