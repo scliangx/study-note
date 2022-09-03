26b9dd170d9a13f19f05a1642739fdd46f10e6d8
scliang-strive <354767929@qq.com>
2022-09-03 14:05:37 +0800

    226-翻转二叉树

---
+++ leetcode/226-翻转二叉树/226-翻转二叉树.go
@@ -0,0 +1,48 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 226-翻转二叉树 深度有限
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	root.Left = right
+	root.Right = left
+	return root
+}
+
+// 广度优先
+func invertTree2(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	var queue []*TreeNode
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			// 反转二叉树结点
+			node.Left, node.Right = node.Right, node.Left
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+	return root
+}
