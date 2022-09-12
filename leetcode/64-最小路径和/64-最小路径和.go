d061b7f80f094593579d6644a68ddbfbae569a40
scliang <354767929@qq.com>
2022-09-12 17:21:57 +0800

    64-最小路径和64-最小路径和

---
+++ leetcode/64-最小路径和/64-最小路径和.go
@@ -0,0 +1,34 @@
+package main
+
+// 64-最小路径和
+func minPathSum(grid [][]int) int {
+	if len(grid) == 0 {
+		return 0
+	}
+	m, n := len(grid), len(grid[0])
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i <= m; i++ {
+		dp[i][0] = grid[i][0] + dp[i-1][0]
+	}
+
+	for j := 1; j <= n; j++ {
+		dp[0][j] = grid[0][j] + dp[0][j-1]
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+	return dp[m][n]
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
