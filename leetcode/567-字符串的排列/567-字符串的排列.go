e1ce815699fe4d6dab83a2b399f0681cd777f525
scliang <354767929@qq.com>
2022-09-24 23:30:07 +0800

    567-字符串的排列

--- leetcode/567-字符串的排列/567-字符串的排列.go
+++ leetcode/567-字符串的排列/567-字符串的排列.go
@@ -2,33 +2,37 @@ package main
 
 // 567-字符串的排列
 func checkInclusion(s1 string, s2 string) bool {
-	s1Map := make(map[byte]int, 0)
-	window := make(map[byte]int, 0)
+	need := make(map[byte]int)
+	window := make(map[byte]int)
 	for i := range s1 {
-		s1Map[s1[i]]++
+		need[s1[i]]++
 	}
-	left, valid := -1, 0
-	for right := 0; right < len(s2); right++ {
+	left,right,valid := 0,0,0
+	for right < len(s2){
 		c := s2[right]
-		window[c]++
-		if window[c] == s1Map[c] {
-			valid++
+		right++
+		// 如果s1 存在c，则添加到滑动窗口Window中
+		if need[c] > 0 {
+			window[c]++
+			// 其中一个字符满足，则valid增加
+			if need[c] == window[c]{
+				valid++
+			}
 		}
-		// 滑动窗口太小
-		if right-left < len(s1) {
-			continue
+		for right - left >= len(s1){
+			if valid == len(need){
+				return true
+			}
+			d := s2[left]
+			left++
+			if need[d] > 0{
+				if need[d] == window[d]{
+					valid--
+				}
+				window[d]--
+			}
 		}
-		// 判断是否符合条件
-		if valid == len(s1Map) {
-			return true
-		}
-		left++
-		d := s2[left]
 
-		if window[d] == s1Map[d] {
-			valid--
-		}
-		window[d]--
 	}
 	return false
 }
