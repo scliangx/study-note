0783f397bd13a309a29e5842c540720021e2b50a
scliang <354767929@qq.com>
2022-09-29 16:41:12 +0800

    归并排序

--- leetcode/常用排序算法/归并排序/归并排序.go
+++ leetcode/常用排序算法/归并排序/归并排序.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+func mergeSort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+	mid := len(nums) / 2
+	left := mergeSort(nums[:mid])
+	right := mergeSort(nums[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+	i, j := 0, 0
+	m, n := len(left), len(right)
+	for i < m && j < n {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}
+
+func main() {
+	nums := []int{8, 9, 5, 7, 1, 2, 5, 7, 6, 3, 5, 4, 8, 1, 8, 5, 3, 5, 8, 4}
+	fmt.Println(mergeSort(nums))
+}
