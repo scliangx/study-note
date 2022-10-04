3ac966d22a174b1ce919d2e147f7f41d5bad2258
scliang <354767929@qq.com>
2022-10-04 17:56:28 +0800

    1541-平衡括号字符串的最少插入次数

--- leetcode/1541-平衡括号字符串的最少插入次数/1541-平衡括号字符串的最少插入次数.go
+++ leetcode/1541-平衡括号字符串的最少插入次数/1541-平衡括号字符串的最少插入次数.go
@@ -1,6 +1,6 @@
 package main
 
-// 1541-平衡括号字符串的最少插入次数
+// 
 func minInsertions(s string) int {
 	res := 0
 	need := 0
@@ -8,7 +8,7 @@ func minInsertions(s string) int {
 		if s[i] == '(' {
 			need += 2
 			// 另外，当遇到左括号时，若对右括号的需求量为奇数，需要插⼊ 1 个右括号。
-			// 因为⼀个左括号需要两个右括 号嘛，右括号的需求必须是偶数
+			// 因为⼀个左括号需要两个右括号，右括号的需求必须是偶数
 			if need%2 == 1 {
 				// 插⼊⼀个右括号
 				res++
